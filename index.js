31193a7077d514c10551095d03101ed4c670c4ce
esEsther <esther.alcu@gmail.com>
2025-12-28 20:31:06 +0100

    'entrega'

--- index.js
+++ index.js
@@ -1,1 +1,214 @@
-//RESUELVE LOS EJERCICIOS AQUI
+
+
+// 1.- Dado el array numbers cuyo valor sea [4, 5, 6, 7, 8, 9, 10], crea 
+// una función elevados que sea el resultado de elevar cada número a si 
+// mismo.
+
+const numbers = [4,5,6,7,8,9,10]
+
+
+const elevados = (array) =>{
+    return array.map((numero) => numero**numero)
+}
+
+const result1 = elevados(numbers)
+
+
+
+//2.- Dado el array foodList con valor ['Pizza', 'Ramen', 
+// 'Paella', 'Entrecot'], generar un segundo array que consiga 
+// generar de salida el resultado esperado.
+
+// [
+//     'Como soy de Italia, amo comer Pizza',
+//     'Como soy de Japón, amo comer Ramen',
+//     'Como soy de Valencia, amo comer Paella',
+//     'Aunque no como carne, el Entrecot es sabroso'
+// ]
+
+
+const foodList = ['Pizza', 'Ramen', 'Paella', 'Entrecot'];
+
+const result2 = foodList.map(comida => {
+    if (comida === 'Pizza') {
+        return 'Como soy de Italia, amo comer Pizza';
+    } else if (comida === 'Ramen') {
+        return 'Como soy de Japón, amo comer Ramen';
+    } else if (comida === 'Paella') {
+        return 'Como soy de Valencia, amo comer Paella';
+    } else {
+        return 'Aunque no como carne, el Entrecot es sabroso';
+    }
+});
+
+// 3.- Dado el array staff, crear un segundo array que forme 
+// frases como en el ejemplo accediendo a las propiedades del 
+// objeto proporcionado:
+
+const staff = [
+  {
+    name: 'Pepe',
+    role: 'The Boss',
+    hobbies: ['leer', 'ver pelis']
+  },
+  {
+    name: 'Ana',
+    role: 'becaria',
+    hobbies: ['nadar', 'bailar']
+  },
+  {
+    name: 'Luis',
+    role: 'programador',
+    hobbies: ['dormir', 'comprar']
+  },
+  {
+    name: 'Carlos',
+    role: 'secretario',
+    hobbies: ['futbol', 'queso']
+  }
+];
+
+// Resultado esperado
+/*
+  [
+    'Pepe es The Boss y le gusta leer y ver pelis',
+    'Ana es becaria y le gusta nadar y bailar',
+    'Luis es programador y le gusta dormir y comprar',
+    'Ana es becaria y le gusta nadar y bailar',
+    'Carlos es secretario y le gusta futbol y queso'
+  ]
+*/
+
+const result3 = staff.map(objeto => {return `${objeto.name} es ${objeto.role} y le gusta ${objeto.hobbies[0]} y ${objeto.hobbies[1]}`})
+// console.log(result3)
+
+//  4.- Crea un segundo array result4 a partir del array numbers2
+//  que devuelva solo los impares
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const result4 = numbers2.filter(numero => numero % 2 !== 0)
+console.log(result4)
+
+//  5.- Dado el array foodList2, genera un segundo array result5 
+// que filtre los platos veganos y saque una sentencia como la 
+// del ejemplo
+// ['Que rico Tempeh me voy a comer!',
+// 'Que rica Tofu burguer me voy a comer!']
+const foodList2 =[
+{
+  name: 'Tempeh',
+  isVeggie: true
+},
+{
+  name: 'Cheesbacon burguer',
+  isVeggie: false
+},
+{
+  name: 'Tofu burguer',
+  isVeggie: true
+},
+{
+  name: 'Entrecot',
+  isVeggie: false
+}];
+
+const result5 = foodList2.filter(objeto => objeto.isVeggie).map(objeto => {
+    if (objeto.name === 'Tempeh'){
+     return `Que rico ${objeto.name} me voy a comer!`  
+    } else {
+      return  `Que rica ${objeto.name} me voy a comer!` 
+    }
+})
+console.log(result5)
+
+// 6.- Dado el array inventory, devolver un array con los 
+// nombres de los elementos que valgan más de 300 euros.
+
+const inventory = [
+  {
+    name: 'Mobile phone',
+    price: 199
+  },
+  {
+    name: 'TV Samsung',
+    price: 459
+  },
+  {
+    name: 'Viaje a Cancún',
+    price: 600
+  },
+  {
+    name: 'Mascarilla',
+    price: 1
+  }
+];
+
+const result6 = inventory.filter(object => object.price > 300).map(object => object.name)
+/*
+  [
+    'TV Samsung',
+    'Viaje a Cancún'
+  ]
+*/
+
+//  6.- Dado el siguiente array numeros [39, 2, 4, 25, 62], 
+// obten la multiplicación de todos los elementos del array
+
+const numeros3 = [39, 2, 4, 25, 62]
+
+const result7 = numeros3.reduce((acc, num) => acc*num,1)
+
+// 7.- Concatena todos los elementos del array con reduce para 
+// que devuelva una sola frase
+
+const sentenceElements = [
+  'Me',
+  'llamo',
+  'Shurmana',
+  'y',
+  'quiero',
+  'sentir',
+  'la',
+  'fuerza',
+  'con',
+  'javascript'
+];
+
+const result8 = sentenceElements.reduce((acc, str) => acc + ' ' + str)
+
+
+// Resultado--> 'Me llamo XX y quiero sentir la fuerza con javascript'
+
+// 8.- Obtener el monto total de los elementos que pertenecen a 
+// catergory "code" en el siguiente array.
+const books = [
+  {
+    name: ' JS for dummies',
+    author: 'Emily A. Vander Veer',
+    price: 20,
+    category: 'code'
+  },
+  {
+    name: 'Don Quijote de la Mancha',
+    author: 'Cervantes',
+    price: 14,
+    category: 'novel'
+  },
+  {
+    name: 'Juego de tronos',
+    author: 'George R. Martin',
+    price: 32,
+    category: 'Fantasy'
+  },
+  {
+    name: 'javascript the good parts',
+    author: 'Douglas Crockford',
+    price: 40,
+    category: 'code'
+  }
+];
+// Resultado --> 60
+
+const result9 = books.filter(b => b.category === 'code')
+.map(b => b.price).reduce((acc, price) => acc + price, 0)
+
